ccc789692d7a7d9ca11c44b88521fa7ae8aa4e11
Hussnain Cheema <hussnain_93@hotmail.com>
2015-03-19 00:52:19 +0500

    Create UnitTest.java

---
+++ UnitTest.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+// Author: Hussnain Shafique Cheema | BSCS2B | 00925
+
+public class UnitTest {
+
+
+	public static void main(String[] args) {
+		 Random rand = new Random();
+
+		int max = 20;
+		int min = 10;
+		int n = rand.nextInt((10 - 5) + 1) + min;
+		int numofLines = 2;
+		int [][]a = new int[numofLines][n];
+		int [][]t = new int[numofLines][n];
+		for(int i=0;i<numofLines;i++) {
+			for(int j=0;j<n;j++) {
+				a[i][j] = rand.nextInt((max - min) + 1) + 1;
+				t[i][j] = rand.nextInt((max - min) + 1) + 1;
+			}
+			
+		} 
+		
+		
+		
+		
+		int e1 = rand.nextInt((max - min) + 1) + 1;
+		int e2 = rand.nextInt((max - min) + 1) + 1;
+		int x1 = rand.nextInt((max - min) + 1) + 1;
+		int x2 = rand.nextInt((max - min) + 1) + 1;
+		
+			System.out.println("Number of stations: " + n);
+			System.out.println("\nLine 1: ");
+			for(int j=0;j<n;j++) {
+				System.out.println("Station: " + (j+1) + " Time : " + a[0][j]);
+			} 
+			System.out.println("\nLine 2: ");
+			for(int j=0;j<n;j++) {
+				System.out.println("Station: " + (j+1) + " Time : " + a[1][j]);
+			} 
+			
+		System.out.println("\n\n");
+		AssemblyLine myob = new AssemblyLine();
+		
+		int finalRes = myob.fastestway(n, e1,e2,x1,x2,a,t);
+		}
+
+}
